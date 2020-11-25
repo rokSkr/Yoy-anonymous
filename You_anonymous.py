c2654b200514e4da1840220f3e343eebf8a227ea
rokSkr <68292269+rokSkr@users.noreply.github.com>
2020-11-25 15:31:30 +0300

    You anoymous

---
+++ You_anonymous.py
@@ -0,0 +1,78 @@
+#You Anonymous?
+stata = "1.Name", "2.Surname", "3.Paul" ,"4.Operator" , "5.Teg-name"
+print("██╗░░░██╗░█████╗░██╗░░░██╗    ░█████╗░███╗░░██╗░█████╗░███╗░░██╗██╗░░░██╗███╗░░░███╗░█████╗░██╗░░░██╗░██████╗░█████╗░")
+print("╚██╗░██╔╝██╔══██╗██║░░░██║    ██╔══██╗████╗░██║██╔══██╗████╗░██║╚██╗░██╔╝████╗░████║██╔══██╗██║░░░██║██╔════╝██╔══██╗")
+print("░╚████╔╝░██║░░██║██║░░░██║    ███████║██╔██╗██║██║░░██║██╔██╗██║░╚████╔╝░██╔████╔██║██║░░██║██║░░░██║╚█████╗░╚═╝███╔╝")
+print("░░╚██╔╝░░██║░░██║██║░░░██║    ██╔══██║██║╚████║██║░░██║██║╚████║░░╚██╔╝░░██║╚██╔╝██║██║░░██║██║░░░██║░╚═══██╗░░░╚══╝░")
+print("░░░██║░░░╚█████╔╝╚██████╔╝    ██║░░██║██║░╚███║╚█████╔╝██║░╚███║░░░██║░░░██║░╚═╝░██║╚█████╔╝╚██████╔╝██████╔╝░░░██╗░░")
+print("░░░╚═╝░░░░╚════╝░░╚═════╝░    ╚═╝░░╚═╝╚═╝░░╚══╝░╚════╝░╚═╝░░╚══╝░░░╚═╝░░░╚═╝░░░░░╚═╝░╚════╝░░╚═════╝░╚═════╝░░░░╚═╝░░")
+#Menu
+print("Start")
+print("Options")
+print("Exit")
+#Start
+comm = input("Enter commands: ")
+if comm == "Start":
+     phonum = int (input ("Enter number phone: "))
+#Options
+if comm == "Options":
+        print("Помочь проекту")
+        print("О проекте")
+        print("О разработчике")
+        print("Exit")
+        options = input("Press commands: ")
+#Help project
+if options == "Помочь проекту":
+     print("Пока денежная помощь не предоставленна проекту,но ты можешь помочь подписавшись на канал в `Discort` просто введи `Discort`")
+     opt = input()
+     if opt == "Discort":
+          print("Спец ссылка для тебя https://discord.gg/6nWDRv8VB3 )")
+#its project
+if options == "О проекте":
+     print("Currently under development")
+#its developer
+if options == "О разработчике":
+     print("Currently under development")
+#ALL Number Phone
+if phonum == int ("79174703151"):
+    print("name:Артур")
+    print("surname:Тимергалиев")
+    print("Paul:M")
+    print("Operator:")
+    print("Teg-Name:Артур Тимергалиев,Артур Тимергалиев-кирюха")
+    print("Addres:Respublic Bashkortosyan")
+if phonum == int ("79177423151"):
+     print(stata)
+     print("1.Артем")
+     print("2.Федосов")
+     print("3.M")
+     print("4.MTC")
+     print("5.Федосов, Артем, Лиза Федосова")
+if phonum == int ("79173533398"):
+     print(stata)
+     print("1.Ирина")
+     print("2.")
+     print("3.Ж")
+     print("МТС")
+     print("Ирина Умиленова, Ирина Анатольевна")
+if phonum == int ("79378410400"):
+     print(stata)
+     print("1.Александр")
+     print("2.Маркевич")
+     print("3.М")
+     print("4.MegaFon")
+     print("5.Александр Маркевич, Сашка Маркевич")
+if phonum == int("79377801317"):
+     print(stata)
+     print("1.Дина \Омар")
+     print("2.Душанбаева\Хаям")
+     print("3.Ж\М")
+     print("4.MegaFon")
+     print("5.Дина Душанбаева, Омар хаям")
+if phonum == int("79379945014"):
+     print(stata)
+     print("1.")
+     print("2.")
+     print("3.")
+     print("4.")
+     print("5.")
